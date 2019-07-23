4241b0394ff43f615463a2bcdd5a0c3ae58b53a7
StratocasterO <omarolmedoferrer@gmail.com>
2019-07-23 14:17:57 +0200

    ejercicios Pablo

---
+++ ejercicios-clase/app/src/main/java/com/example/user/myapplication/Planta.java
@@ -0,0 +1,33 @@
+package com.example.user.myapplication;
+
+// Esta clase es un POJO (Plain Old Java Object)
+
+public class Planta {
+    private int id;
+    private String name;
+    private Float price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+}
